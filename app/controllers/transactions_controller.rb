ad893d302b0c383fb7bf4e01abcad0592b5d0d35
Mondego90 <joaogranja10@hotmail.com>
2016-02-10 22:06:52 +0000

    Finished dashboard

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,16 +2,16 @@ class TransactionsController < ApplicationController
 
 	def create
 		book = Book.find_by!(slug: params[:slug])
-		@sale = book.sales.create!(
+		sale = book.sales.create!(
 			amount: book.price,
 			buyer_email: current_user.email,
 			seller_email: book.user.email,
 			stripe_token: params[:stripeToken]
 			)
-		@sale.process!
+		sale.process!
 	
-		if @sale.finished?
-			redirect_to pickup.url(guid: @sale.guid), notice: "Transaction successful"
+		if sale.finished?
+			redirect_to pickup_url(guid: sale.guid), notice: "Transaction successful"
 		else
 			redirect_to book_path(book), notice: "Something went wrong"
 		end
